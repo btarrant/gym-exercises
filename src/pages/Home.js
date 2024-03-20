9cd7391ad46b6a4a4ab9d80da3a5d5d8aab02f36
Brandon Tarrant <brandontarrant04@gmail.com>
2024-03-19 21:36:15 -0400

    Implement horizontal scrolling for search

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import { Box } from "@mui/material";
+import Exercises from "../components/Exercises";
+import HeroBanner from "../components/HeroBanner";
+import SearchExercises from "../components/SearchExercises";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [bodyPart, setBodyPart] = useState("all");
+  const [exercises, setExercises] = useState([]);
+
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+      <Exercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+    </Box>
+  );
 };
 
 export default Home;
