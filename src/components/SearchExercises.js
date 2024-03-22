ba5c0e269783e88bd748555867ba47e28e4260e7
Brandon Tarrant <brandontarrant04@gmail.com>
2024-03-21 20:22:20 -0400

    Fixed FetchData case

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Box, Button, Stack, TextField, Typography } from "@mui/material";
-import { exerciseOptions, fetchData } from "../utils/fetchData";
+import { exerciseOptions, fetchData } from "../utils/FetchData";
 import HorizontalScrollbar from "./HorizontalScrollbar";
 
 const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
